b943c468dc319b2ecfff69a6f065ca51df1eab28
N1Alice <kc700370@gmail.com>
2023-12-11 07:18:34 +0900

    join form / RequestMapping-MCon

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -3,10 +3,10 @@ package com.ll.medium.domain.member.member.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RestController("/member")
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     @GetMapping("/join")
