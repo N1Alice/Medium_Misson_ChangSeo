657f58129a8f3e1e3d206a41d9c5dddc4f9677ea
N1Alice <kc700370@gmail.com>
2024-01-07 16:55:27 +0900

    Chapter 28 work

---
+++ src/main/java/com/ll/medium/domain/post/post/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.ll.medium.domain.post.post.controller;
+
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.domain.post.post.service.PostService;
+import com.ll.medium.global.rq.Rq;
+import com.ll.medium.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/post")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+    private final Rq rq;
+
+    @GetMapping("/{id}")
+    public String showPost(@PathVariable Long id) {
+        rq.setAttribute("post", postService.findById(id).get());
+        return "domain/post/post/detail";
+    }
+}
